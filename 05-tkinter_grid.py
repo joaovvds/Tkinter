7db67be7b408ff66e91bd3b3bceae583ac0d24f4
joaovvds <jaocloud97@gmail.com>
2025-10-26 20:25:59 -0300

    aula 05 concluida

---
+++ 05-tkinter_grid.py
@@ -0,0 +1,64 @@
+import tkinter as tk
+
+def saudar_utilizador():
+    nome_digitado = campo_nome.get()
+
+    if (nome_digitado):
+        mensagem = f"Olá, {nome_digitado}! Bem vindo ao Tkinter."
+        rotulo_saudacao.config(text="Nome digitado com Sucesso", fg="green")
+    else: 
+        mensagem = "Por favor, digite seu nome para ser saudado!"
+        rotulo_saudacao.config(text="Você não digitou seu nome", fg="red")
+    
+    rotulo_output.config(text=mensagem)
+    
+root = tk.Tk()
+root.title("Tkinter com Grid layout")
+# Não definiremos um tamanho fixo (geometry) para ver o grid em ação!
+
+# --- 3. CRIAÇÃO E POSICIONAMENTO DOS WIDGETS COM GRID ---
+
+# 3.1. Rótulo de instrução (Label)
+rotulo_instrucao = tk.Label(text="Digite seu nome:")
+
+# row=0: primeira linha
+# column=0: primeira coluna
+# sticky="W": anexa o widget ao lado Oeste (West/Esquerda) da célula
+rotulo_instrucao.grid(row=0, column=0, padx=10, pady=10, sticky="W")
+
+
+# 3.2 Campo de Entrada de Texto (Entry)
+campo_nome = tk.Entry(root, width=30)
+
+# row=0: primeira linha (ao lado do rótulo)
+# column=1: segunda coluna
+# sticky="WE": anexa o widget aos lados Oeste e Este, fazendo-o preencher a largura da coluna
+campo_nome.grid(row=0, column=1, padx=10, pady=10, sticky="WE")
+
+# 3.2.9 Rótulo de Output (Label)
+rotulo_output = tk.Label(root, text="") #saida output para o nome
+rotulo_output.grid(row=2, columnspan=2, padx=10, pady=10)
+
+# 3.3. Botão
+botao_saudar = tk.Button(root, text="Saudar utilizador", command=saudar_utilizador)
+
+# row=1: segunda linha
+# columnspan=2: faz com que o botão ocupe 2 colunas (coluna 0 e coluna 1)
+# sticky="WE": faz o botão preencher toda a largura das duas colunas
+botao_saudar.grid(row=1, columnspan=2)
+
+
+# 3.4. Rótulo de Output (Label)
+rotulo_saudacao = tk.Label(root, text="Aguardando nome...", fg="darkgreen")
+
+# row=2: terceira linha
+# columnspan=2: também ocupa as 2 colunas
+rotulo_saudacao.grid(row=3, columnspan=3, padx=10, pady=10)
+
+
+# 4. Configuração extra do Grid (Melhorando a responsividade)
+# Faz com que a coluna 1 se expanda quando a janela é redimensionada
+root.grid_columnconfigure(1, weight=1)
+
+root.mainloop()
+print("Programa finalizado.")
